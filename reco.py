212b6be623eb0a9031f02342e16349465d9ed690
Victor Sarrís Silva Santos <devvictorsarris@gmail.com>
2025-09-23 09:02:43 -0300

    menor indice de camera

--- reco.py
+++ reco.py
@@ -4,7 +4,7 @@ import mediapipe as mp
 # inicializar o open cv e o mediapipe
 webcam = cv.VideoCapture(0)  # ajuste o índice da câmera
 solucao_reconhecimento = mp.solutions.face_detection
-reconhecedor_rostos = solucao_reconhecimento.FaceDetection(min_detection_confidence=0.8)
+reconhecedor_rostos = solucao_reconhecimento.FaceDetection(min_detection_confidence=0.7)
 desenho = mp.solutions.drawing_utils
 
 while True:
@@ -21,7 +21,7 @@ while True:
     if lista_rostos.detections:
         for rosto in lista_rostos.detections:
             score = rosto.score[0]
-            if score > 0.8:  # evita falsos positivos
+            if score > 0.7:  # evita falsos positivos
                 desenho.draw_detection(frame, rosto)
                 cv.putText(frame, f"{int(score*100)}%", (50, 50),
                            cv.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
